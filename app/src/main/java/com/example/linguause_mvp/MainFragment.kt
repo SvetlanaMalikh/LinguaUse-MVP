723f8cd13d47bcb763f3fcb2eb988e78086e6396
SvetlanaMalikh <svetlankaa2018@gmail.com>
2025-06-11 09:55:56 +0300

    Добавлен MainFragment.kt

---
+++ app/src/main/java/com/example/linguause_mvp/MainFragment.kt
@@ -0,0 +1,17 @@
+class MainFragment : Fragment(R.layout.fragment_main) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.btn_words).setOnClickListener {
+            findNavController().navigate(R.id.wordGameFragment)
+        }
+
+        view.findViewById<Button>(R.id.btn_sentences).setOnClickListener {
+            findNavController().navigate(R.id.sentenceGameFragment)
+        }
+
+        view.findViewById<Button>(R.id.btn_dictionary).setOnClickListener {
+            findNavController().navigate(R.id.dictionaryFragment)
+        }
+    }
+}
